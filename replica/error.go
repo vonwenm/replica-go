01649376d80d7931e7635dae87de1363af13753b
Birol Bilgin <birolbilgin@gmail.com>
2015-02-20 15:13:06 +0200

    exported HTTPError

--- replica/error.go
+++ replica/error.go
@@ -6,17 +6,18 @@ import (
 	"strings"
 )
 
-type httperror struct {
+// HTTPError http status code and error message
+type HTTPError struct {
 	Code    int    `json:"error_code"`
 	Message string `json:"error_message"`
 }
 
-func (r *httperror) Error() string {
+func (r *HTTPError) Error() string {
 	return fmt.Sprintf("http error: Code: %d Message: %s", r.Code, r.Message)
 }
 
-func newHTTPError(code int, msg []byte) *httperror {
-	herr := &httperror{}
+func newHTTPError(code int, msg []byte) *HTTPError {
+	herr := &HTTPError{}
 	err := json.Unmarshal(msg, herr)
 	if err != nil || herr.Code != code {
 		herr.Code = code
